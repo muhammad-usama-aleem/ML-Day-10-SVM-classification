2a0bc4f1774ff1f61fdea760c56678fa252ef5e7
Muhammmad Usama Aleem <56190389+muhammad-usama-aleem@users.noreply.github.com>
2021-08-18 14:38:32 +0500

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -25,10 +25,10 @@ sc_x = StandardScaler()  # scaling x if necessary
 x_train = sc_x.fit_transform(x_train)
 x_test = sc_x.transform(x_test)
 
-# fitting the classifier model into dataset
-from sklearn.neighbors import KNeighborsClassifier
+# fitting the regression model into dataset
+from sklearn.svm import SVC
 
-classifier = KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2)
+classifier = SVC(kernel='linear', random_state=0)
 classifier.fit(x_train, y_train)
 
 # Predicting a new result with Linear Regression
@@ -37,26 +37,24 @@ y_predict = classifier.predict(x_test)
 # to compare 2 matrices side by side
 """np.column_stack((y_predict, y_test))"""
 
-# making con fusion matrice
+#making confusion matrice
 from sklearn.metrics import confusion_matrix
-
 cm = confusion_matrix(y_test, y_predict)
 print(cm)
 
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
-
 x_set, y_set = x_train, y_train
-x1, x2 = np.meshgrid(np.arange(start=x_set[:, 0].min() - 1, stop=x_set[:, 0].max() + 1, step=0.01),
-                     np.arange(start=x_set[:, 1].min() - 1, stop=x_set[:, 1].max() + 1, step=0.01))
+x1, x2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))
 plt.contourf(x1, x2, classifier.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape),
-             alpha=0.75, cmap=ListedColormap(('red', 'green')))
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
 plt.xlim(x1.min(), x1.max())
 plt.ylim(x2.min(), x2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],
-                c=ListedColormap(('red', 'green'))(i), label=j)
-plt.title('KNN (Training set)')
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
@@ -64,18 +62,17 @@ plt.show()
 
 # Visualising the Test set results
 from matplotlib.colors import ListedColormap
-
 x_set, y_set = x_test, y_test
-x1, x2 = np.meshgrid(np.arange(start=x_set[:, 0].min() - 1, stop=x_set[:, 0].max() + 1, step=0.01),
-                     np.arange(start=x_set[:, 1].min() - 1, stop=x_set[:, 1].max() + 1, step=0.01))
+x1, x2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))
 plt.contourf(x1, x2, classifier.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape),
-             alpha=0.75, cmap=ListedColormap(('red', 'green')))
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
 plt.xlim(x1.min(), x1.max())
 plt.ylim(x2.min(), x2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],
-                c=ListedColormap(('red', 'green'))(i), label=j)
-plt.title('KNN (Test set)')
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
